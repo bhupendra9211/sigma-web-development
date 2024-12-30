c71f5fc183ba029718c1da9083f1b4ac86286314
bhupendra9211 <shahbhupendra9211@gmail.com>
2024-12-30 14:46:09 +0545

    function in js

---
+++ function.js
@@ -0,0 +1,33 @@
+// console.log("how are you bhuupendrsa");
+
+// function nice(name) {
+//     console.log("hey " + name + " How are  you");
+// }
+
+// nice("harry")
+// nice("bhupendra")
+
+// console.log("how are you \n bhuo")
+
+// function sum(a,b) {
+//     cd = (a+b);
+//     return cd;
+// }
+
+// c = sum(1,2);
+// console.log(c);
+
+// d = sum(33,44);
+// console.log(d);
+
+// const func = () =>{
+//     console.log("this is arrow function")
+// }
+
+// func();
+
+const func = (x) =>{
+    console.log("you pass the value is ", x);
+}
+
+func(22);
