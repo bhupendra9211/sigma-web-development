8edf17d7faa91101d343bb26444e424385e97de9
bhupendra9211 <shahbhupendra9211@gmail.com>
2024-12-30 12:11:23 +0545

    basic java script

---
+++ java_script.js
@@ -0,0 +1,31 @@
+// alert("hello world");
+
+// console.log("Hello how are you");
+
+
+// let num1 = prompt("Enter the first number");
+// console.log("your first number is: " + num1);
+
+// document.title = "first page";
+
+let x = "bhupendra";
+let y = 33;
+let z = 3.33;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(typeof x);
+console.log(x,y,z,p,q,r);
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r);
+
+let obj = {
+    name: "bhupendra",
+    "job role": "developer",
+    "is_handsome": true,
+}
+console.log(obj);
+console.log(obj["job role"]);
+
+obj.salary ="100000";
+console.log(obj);
