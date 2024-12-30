0897dd5e5529159c5f0f9ae4249eb342fabddf46
bhupendra9211 <shahbhupendra9211@gmail.com>
2024-12-30 13:37:58 +0545

    condion operator in js

---
+++ condition.js
@@ -0,0 +1,34 @@
+// console.log("hello from condition in javascript")
+
+// let age = 45;
+// let age = prompt("Enter your age");
+
+// if(age>18){
+//     console.log("you can drive");
+// }
+// else{
+//     console.log("you can't drive");
+// }
+
+
+// let num1 = "3";
+// let num2 = 3;
+
+// if(num1 == num2){
+//     console.log("true")
+// }
+// else{
+//     console.log("false")
+// }
+// if(num1 === num2){
+//     console.log("true")
+// }
+// else{
+//     console.log("false")
+// }
+
+let num1 = 50;
+let num2 = 44;
+
+let c = num1 > num2 ? (num1-num2) : (num2 - num1);
+console.log(c);
