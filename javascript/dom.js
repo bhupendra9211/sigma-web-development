af7b25c63908db81ff804c6a95bcd22e2ba86d78
bhupendra9211 <shahbhupendra9211@gmail.com>
2025-01-02 22:37:26 +0545

    dom child parent sibling

---
+++ javascript/dom.js
@@ -0,0 +1,6 @@
+console.log("this is dom child parent sibling");
+document.title = "hello"
+
+console.log(document.body.childNodes)
+console.log(document.body.childNodes[0])
+console.log(document.body.childNodes[1])
