3a3a28602b24788c7c9a595f084b22c8a034d0e0
bhupendra9211 <shahbhupendra9211@gmail.com>
2025-01-03 20:51:29 +0545

    automate the color changes

--- javascript/random_color.js
+++ javascript/random_color.js
@@ -1,7 +1,23 @@
-document.querySelectorAll(".box").forEach((e,i) => {
-    let colors = ['red','green','black','yellow','blue'];
-    let randomColor = colors[Math.floor(Math.random()*colors.length)];
-    // let randomColor = colors[i];
-    console.log(randomColor);
-    e.style.backgroundColor = randomColor;
+function randomColor(){
+    let code1 = Math.floor(Math.random()*100);
+    let code2 = Math.floor(Math.random()*100);
+    let code3 = Math.floor(Math.random()*100);
+    let rgb = [code1,code2,code3]
+
+    return rgb;
+}
+
+// let color = randomColor();
+// console.log(color);
+document.querySelectorAll(".box").forEach((e) => {
+    let color = randomColor();
+    e.style.backgroundColor = `rgb(${color[0]},${color[1]},${color[2]})`;
 })
+
+// document.querySelectorAll(".box").forEach((e,i) => {
+//     let colors = ['red','green','black','yellow','blue'];
+//     let randomColor = colors[Math.floor(Math.random()*colors.length)];
+//     // let randomColor = colors[i];
+//     console.log(randomColor);
+//     e.style.backgroundColor = randomColor;
+// })
---
+++ javascript/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+erySelectorAll(".box").forEach((e,i) => {
+//     let color = randomColor();
+//     e.style.backgroundColor = `rgb(${i},${i},${i})`;
+// })
