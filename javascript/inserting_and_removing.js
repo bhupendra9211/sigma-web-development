653f449b9f0eb29b269329ecbaf9a1b583521811
bhupendra9211 <shahbhupendra9211@gmail.com>
2025-01-04 13:55:52 +0545

    inserting and removing element using js

---
+++ javascript/inserting_and_removing.js
@@ -0,0 +1,42 @@
+// document.querySelector(".box")
+// console.log(document.querySelector(".container").innerHTML)
+// console.log(document.querySelector(".container").innerText)
+// console.log(document.querySelector(".box").innerHTML)
+// console.log(document.querySelector(".box").innerText)
+// console.log(document.querySelector(".container").outerHTML);
+// console.log(document.querySelector(".container").tagName);
+// console.log(document.querySelector(".container").nodeName);
+// console.log(document.querySelector(".box").hidden);
+// document.querySelector(".box").hidden = true;
+// document.querySelector(".box").innerHTML = "hey i  am bhupsd"
+// document.querySelector(".container").innerHTML = "<div>fdkjd</div>"
+// document.querySelector(".container").hasAttribute("style")
+// document.querySelector(".container").getAttribute("style")
+// document.querySelector(".container").setAttribute("style","padding:5px")
+// document.querySelector(".container").removeAttribute("style")
+
+// document.designMode = "on";
+// document.querySelector(".box").dataset
+
+// Inserting mode 
+
+// document.querySelector(".container").innerHTML = "<div class = \"box\">dkjldjklfjdf</div>"
+
+// let div = document.createElement("div");
+// div.innerHTML = "i have been inserted <b> inside container </b>";
+// div.setAttribute("class","box");
+// document.querySelector(".container").append(div);
+// document.querySelector(".container").before(div);
+// document.querySelector(".container").after(div);
+
+// let cont = document.querySelector(".container");
+// cont.insertAdjacentHTML("afterend","<b>dfjld</b>")
+// cont.insertAdjacentHTML("afterend","<div class =\"box\">kdjklfjd</div>")
+// document.querySelector(".box").remove()
+
+// document.querySelector(".container").classList
+// document.querySelector(".container").className
+// document.querySelector(".container").classList.add(".bhups")
+// document.querySelector(".container").classList.remove(".bhups")
+
+// document.querySelector(".box").classList.toggle("red_box")
